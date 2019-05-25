841f4771a30c8ba3ba84ccf200926c657b2081c6
kchirkov <51020519+kchirkov@users.noreply.github.com>
2019-05-25 14:02:38 +0300

    Add files via upload

---
+++ pr11/pr11.cpp
@@ -0,0 +1,15 @@
+﻿
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char name[20];
+	cout << "Enter your name!" << endl;
+	cin.getline(name, 19);
+	cout << "Hello, " << name << "!" << endl;
+
+	return 0;
+}
