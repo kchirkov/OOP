8c19cb7483c6940e7cf3c95fca2b9c7d36ad2db1
kchirkov <51020519+kchirkov@users.noreply.github.com>
2019-05-25 14:31:51 +0300

    Add files via upload

---
+++ pr24/pr24.cpp
@@ -0,0 +1,30 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	double a, x, y, t;
+	a = 3.5;
+	x = -0.7;
+	if (a <= x)
+		y = a + log(x + a);
+	else
+		y = sqrt(sin(a*x));
+	if (y < a)
+		t = y / (a - x);
+	if (y = a)
+		t = y / (a - x) + (a + x) / (y*y);
+	if (y > a)
+		t = tan(a*x) + cos(2 * a*y);
+	cout << "a = " << a << endl;
+	cout << "x = " << x << endl;
+	cout << "y = " << y << endl;
+	cout << "t = " << t << endl;
+
+	system("pause");
+	return 0;
+}
