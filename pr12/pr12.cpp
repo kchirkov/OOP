473f527126a79694cb7ce382d21c8353c4833355
kchirkov <51020519+kchirkov@users.noreply.github.com>
2019-05-25 14:10:41 +0300

    Add files via upload

---
+++ pr12/pr12.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double a, b, s, r, u, d;
+	cout << "Vvedite 2 chisla v poryadke ubivaniya" << endl;
+	cin >> a >> b;
+	s = a + b;
+	r = a - b;
+	u = a * b;
+	if (b != 0)
+		d = a / b;
+	cout << "Summa = " << s << endl;
+	cout << "Raznost = " << r << endl;
+	cout << "Umnozhenie = " << u << endl;
+	cout << "Delenie = " << d << endl;
+
+	return 0;
+}
+
+
