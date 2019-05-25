e652196f65dc355ad1e42ea56e8aec9119c59176
kchirkov <chirkovchanel@gmail.com>
2019-05-25 19:17:22 +0300

    Update pr35.cpp

--- pr35/pr35.cpp
+++ pr35/pr35.cpp
@@ -16,24 +16,27 @@ int main()
 
 	cout << "Enter year: ";
 	cin >> year;
+	cin.ignore();
 	book1.setyear(year);
 
 	cout << "Enter title: ";
-	cin >> title;
+	getline(cin, title);
 	book1.settitle(title);
 
 	lib.addBook(book1);
 	cout << "Second book:"<<endl;
 	cout << "Enter author: ";
-	cin >> author;
+	getline(cin, author);
 	book2.setauthor(author);
 
 	cout << "Enter year: ";
 	cin >> year;
+	cin.clear();
+	cin.ignore();
 	book2.setyear(year);
 
 	cout << "Enter title: ";
-	cin >> title;
+	getline(cin, title);
 	book2.settitle(title);
 
 	cout << endl;
