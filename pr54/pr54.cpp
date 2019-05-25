a131e8f10ad50eb8fe1127e05817b9b987dd96d6
kchirkov <51020519+kchirkov@users.noreply.github.com>
2019-05-25 15:19:11 +0300

    Add files via upload

---
+++ pr54/pr54.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int sum(int a, int b)
+{
+	return a + b;
+}
+
+int raz(int a, int b)
+{
+	return a - b;
+}
+
+double sum(double a, double b)
+{
+	return a + b;
+}
+
+double raz(double a, double b)
+{
+	return a - b;
+}
+
+int main()
+{
+
+	int s1 = sum(1, 2);
+	int r1 = raz(2, 1);
+	double s2 = sum(1.1, 2.2);
+	double r2 = raz(2.2, 1.1);
+	cout <<"sum and raz int chisel"<<endl<< s1 << endl << r1 << endl<<"sum and raz double chisel "<<endl << s2 << endl << r2 << endl;
+	return 0;
+}
