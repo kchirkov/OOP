7f1d3c02f8f94143c1555a6257574d1edf1cf946
kchirkov <51020519+kchirkov@users.noreply.github.com>
2019-05-25 15:13:10 +0300

    Add files via upload

---
+++ pr53/pr53.cpp
@@ -0,0 +1,61 @@
+﻿
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+
+void insertSort(int *mass, int n)
+{
+	for (int i = 1; i < n; i++)
+		for (int j = i; j > 0 && mass[j - 1] > mass[j]; j--)
+			swap(mass[j - 1], mass[j]);
+}
+
+
+void insertSort(double *mass, int n)
+{
+	for (int i = 1; i < n; i++)
+		for (int j = i; j > 0 && mass[j - 1] > mass[j]; j--)
+			swap(mass[j - 1], mass[j]);
+}
+
+
+
+
+int main()
+{
+
+	int n;
+	cout << "Count of elements: ";
+	cin >> n;
+
+
+	double* mass;
+	mass = new double[n];
+	for (int i = 0; i < n; ++i)
+	{
+		cout << i + 1 << " element ";
+		cin >> mass[i];
+	}
+
+
+	cout << "Array: ";
+	for (int i = 0; i < n; ++i)
+	{
+		cout << mass[i] << " ";
+	}
+	cout << endl;
+
+	insertSort(mass, n);
+	cout << "Sorted array: ";
+	for (int i = 0; i < n; ++i)
+	{
+		cout << mass[i] << " ";
+	}
+	cout << endl;
+
+	delete[] mass;
+	system("pause");
+	return 0;
+}
