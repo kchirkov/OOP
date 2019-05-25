44883f2718c415ab7c9a33f50bbc828350bc2714
kchirkov <51020519+kchirkov@users.noreply.github.com>
2019-05-25 15:35:40 +0300

    Add files via upload

---
+++ pr72/pr72.cpp
@@ -0,0 +1,40 @@
+﻿
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class figure {
+	int corners;
+public:
+	figure(int scorners) : corners(scorners) {}
+	virtual void getsquareformula() = 0;
+};
+
+class chug : public figure {
+public:
+	chug() : figure(4) {}
+	void getsquareformula() = 0;
+};
+
+class romb : public chug {
+public:
+	void getsquareformula() { cout << "d1*d2/2" << endl; }
+};
+
+class pryamoug : public chug {
+public:
+	void getsquareformula() { cout << "a*b" << endl; }
+};
+
+int main()
+{
+	romb a;
+	pryamoug b;
+	a.getsquareformula();
+	b.getsquareformula();
+	system("pause");
+	return 0;
+}
